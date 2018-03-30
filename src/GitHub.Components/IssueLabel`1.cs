fad66babcab4bf052a3a8a1695dccbf496ba9f09
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2018-03-30 14:59:47 +0300

    Update ControlDefinition of IssueLabel<TOwner>

--- src/GitHub.Components/IssueLabel`1.cs
+++ src/GitHub.Components/IssueLabel`1.cs
@@ -2,7 +2,7 @@
 
 namespace GitHub.Components
 {
-    [ControlDefinition("a", ContainingClass = "label", ComponentTypeName = "label")]
+    [ControlDefinition("a", ContainingClass = "IssueLabel", ComponentTypeName = "label")]
     public class IssueLabel<TOwner> : Content<string, TOwner>
         where TOwner : PageObject<TOwner>
     {
